c704ca3a38dada776fc010a30ba948c0d4e83de4
Abel Masila <abel@x2ai.com>
2022-02-17 02:46:57 +0300

    actions used to submit form

--- app/routes/posts/new.jsx
+++ app/routes/posts/new.jsx
@@ -1,8 +1,38 @@
+import { Link, redirect } from 'remix';
+export const action = async ({ request }) => {
+  const form = await request.formData();
+  const title = form.get('title');
+  const body = form.get('body');
+  const post = { title, body };
+  console.log(post);
+  return redirect('/posts');
+};
+
 function New() {
   return (
-    <div>
-      <h1>New post here..</h1>
-    </div>
+    <>
+      <div className="page-header">
+        <h1>New Post</h1>
+        <Link to="posts" className="btn btn-reverse">
+          Back
+        </Link>
+      </div>
+      <div className="page-content">
+        <form method="POST">
+          <div className="form-control">
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" id="title" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="body">Post Body</label>
+            <textarea id="body" name="body" />
+          </div>
+          <button className="btn btn-block" type="submit">
+            Add Post
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
 
