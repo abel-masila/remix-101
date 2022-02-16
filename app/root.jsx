84960de2b0051109c007cc53e978892c7bece5de
Abel Masila <abel@x2ai.com>
2022-02-17 02:22:14 +0300

    added styles and meta info

--- app/root.jsx
+++ app/root.jsx
@@ -1,9 +1,23 @@
-import { Outlet, LiveReload } from 'remix';
+import { Outlet, LiveReload, Link, Links, Meta } from 'remix';
 
+import globalStylesUrl from '~/styles/global.css';
+
+export const links = () => [{ rel: 'stylesheet', href: globalStylesUrl }];
+export const meta = () => {
+  const description = 'A cool blog built with Remix';
+  const keywords = 'remix, blog, react, javascript';
+
+  return {
+    description,
+    keywords,
+  };
+};
 export default function App() {
   return (
     <Document title="Blog App">
-      <Outlet />
+      <Layout>
+        <Outlet />
+      </Layout>
     </Document>
   );
 }
@@ -12,6 +26,10 @@ function Document({ children, title }) {
   return (
     <html lang="en">
       <head>
+        <Links />
+        <Meta />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>{title}</title>
       </head>
       <body>
@@ -19,5 +37,23 @@ function Document({ children, title }) {
         {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
       </body>
     </html>
+  );
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          Remix
+        </Link>
+        <ul className="nav">
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="container">{children}</div>
+    </>
   );
 }
