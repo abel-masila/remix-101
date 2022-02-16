0cd96f9be3e281f74aa31c273b5ddda352e34a16
Abel Masila <abel@x2ai.com>
2022-02-17 02:25:12 +0300

    setup posts route

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Welcome</h1>;
+  return (
+    <>
+      <h1>Welcome</h1>
+      <p>Building with remix is fun</p>
+    </>
+  );
 }
---
+++ app/routes/posts.jsx
@@ -0,0 +1,9 @@
+function Posts() {
+  return (
+    <div>
+      <h1>Post Route here..</h1>
+    </div>
+  );
+}
+
+export default Posts;
