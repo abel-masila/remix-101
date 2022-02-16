bc935cd3eaba65c468726890b56478291fbbb281
Abel Masila <abel@x2ai.com>
2022-02-17 02:27:44 +0300

    nested routes

--- app/routes/posts.jsx
+++ app/routes/posts.jsx
@@ -1,7 +1,9 @@
+import { Outlet } from 'remix';
 function Posts() {
   return (
     <div>
       <h1>Post Route here..</h1>
+      <Outlet />
     </div>
   );
 }
---
+++ app/routes/posts/new.jsx
@@ -0,0 +1,9 @@
+function New() {
+  return (
+    <div>
+      <h1>New post here..</h1>
+    </div>
+  );
+}
+
+export default New;
