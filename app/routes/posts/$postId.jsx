f57491646b74fad0242502e89ff025c80209ae8c
Abel Masila <abel@x2ai.com>
2022-02-17 02:31:14 +0300

    base folder set

---
+++ app/routes/posts/$postId.jsx
@@ -0,0 +1,13 @@
+import { useParams } from 'remix';
+
+function Post() {
+  const params = useParams();
+
+  return (
+    <div>
+      <h1>Post: {params.postId}</h1>
+    </div>
+  );
+}
+
+export default Post;
---
+++ app/routes/posts/index.jsx
@@ -0,0 +1,9 @@
+function PostItems() {
+  return (
+    <div>
+      <h1>Posts</h1>
+    </div>
+  );
+}
+
+export default PostItems;
