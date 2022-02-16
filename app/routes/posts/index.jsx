db31e611c0d6b0d9c258133e3209083df330201e
Abel Masila <abel@x2ai.com>
2022-02-17 02:39:01 +0300

    useLoaderData setup

--- app/routes/posts/index.jsx
+++ app/routes/posts/index.jsx
@@ -1,7 +1,47 @@
+import { useLoaderData, Link } from 'remix';
+
+export const loader = () => {
+  const data = {
+    posts: [
+      {
+        id: 1,
+        title: 'Post 1',
+        body: 'This is post 1',
+      },
+      {
+        id: 2,
+        title: 'Post 2',
+        body: 'This is post 2',
+      },
+      {
+        id: 3,
+        title: 'Post 3',
+        body: 'This is post 3',
+      },
+    ],
+  };
+  return data;
+};
 function PostItems() {
+  const { posts } = useLoaderData();
   return (
     <div>
-      <h1>Posts</h1>
+      <div className="page-header">
+        <h1>Posts</h1>
+        <Link to="/posts/new" className="btn">
+          New Page
+        </Link>
+      </div>
+
+      <ul className="posts-list">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={post.id}>
+              <h3>{post.title}</h3>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
