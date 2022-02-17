607aecd0cfc43ad71d6256c945b959ddb3ae325c
Abel Masila <abel@x2ai.com>
2022-02-18 00:04:40 +0300

    associate new post with user

--- app/routes/posts/new.jsx
+++ app/routes/posts/new.jsx
@@ -1,5 +1,6 @@
 import { Link, redirect, useActionData, json } from 'remix';
 import { db } from '~/utils/db.server';
+import { getUser } from '~/utils/session.server';
 
 function validateTitle(title) {
   if (typeof title !== 'string' || title.length < 3) {
@@ -19,6 +20,8 @@ export const action = async ({ request }) => {
   const form = await request.formData();
   const title = form.get('title');
   const body = form.get('body');
+
+  const user = await getUser(request);
   const fields = { title, body };
 
   const fieldErrors = {
@@ -30,7 +33,12 @@ export const action = async ({ request }) => {
     return badRequest({ fieldErrors, fields });
   }
   //submit post to database
-  const post = await db.post.create({ data: fields });
+  const post = await db.post.create({
+    data: {
+      ...fields,
+      userId: user.id,
+    },
+  });
 
   return redirect(`/posts/${post.id}`);
 };
